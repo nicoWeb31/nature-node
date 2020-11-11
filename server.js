2cc7a74b0f7bb9f85d0bc28d04d88d86d2c6a5bc
nicoWeb31 <nico.riot@free.fr>
2020-11-11 13:21:12 +0100

    add and config moongose

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const mongoose =require('mongoose');
 const dotenv = require('dotenv');//gestion variable d'env
 dotenv.config({path:'./config.env'})
 
 const app = require('./app');
 
 //variable d'environenemt
-
 //console.log(app.get('env'))
 //console.log(process.env)
+
+
+//mongoose
+const db = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+mongoose.connect(db,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}).then(con=>{
+    //console.log(con.connections)
+    console.log('connect to mongodb 🙂 ')
+})
+
+
 
 //-------------port--------------------------------
 const port = process.env.PORT || 3003;
