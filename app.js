1d8606501b596e31b7f7e21e80a8274177ca234a
nicoWeb31 <nico.riot@free.fr>
2020-11-14 09:59:54 +0100

    Custom error class

--- app.js
+++ app.js
@@ -32,10 +32,28 @@ app.use('/api/v1/users',require('./routes/userRoutes'));
 
 //autres route 404
 app.all('*',function(req,res,next){
-    res.status(404).json({
-        status: 'fail',
-        message:`Can't find ${req.originalUrl} on this server !`
-    });
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message:`Can't find ${req.originalUrl} on this server !`
+    // });
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server !`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+
+})
+
+//err midlleware
+app.use((err, req, res, next) => {
+    
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    })
 })
 
 
