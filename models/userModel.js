fbeb72edb391f9dce611160858d0f2be2a6cf3ea
nicoWeb31 <nico.riot@free.fr>
2020-11-15 19:35:12 +0100

    update user modele with bcrypt hash and validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,55 @@
-const validator = require('validator');
+const validator = require("validator");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Shema = mongoose.Schema;
+
 
 const userShema = new Shema({
     name: {
         type: String,
-        required: [true,'Please tell us your name ! ']
+        required: [true, "Please tell us your name ! "],
     },
     email: {
         type: String,
         unique: true,
-        required: [true,'Please provide your email ! '],
+        required: [true, "Please provide your email ! "],
         lowercase: true,
-        validate:{
-            validator: validator.isEmail,
-            message: 'Please provide an vilde email address !'
-        }
+        validate: [validator.isEmail, "Please provide a valid email ! "],
     },
     photo: {
-        type: String
+        type: String,
     },
     password: {
         type: String,
-        required:[true,'Please provide a password !'],
-        minlength: 8
+        required: [true, "Please provide a password !"],
+        minlength: 8,
     },
     passwordConfirm: {
         type: String,
-        required: [true,'Please comfirn your password !']
+        required: [true, "Please comfirn your password !"],
+        validate: {
+            //only work on create and save !!!
+            validator: function (el) {
+                return el === this.password; //return true or false renvoie une error
+            },
+            message: "Password doesn't match !!",
+        },
     },
 });
 
 
-const User = mongoose.model('User',userShema);
+//mongose middleware, pre-save for passwordencrypte
+userShema.pre('save',async function(next){
+    //only run this function if password was actualy modified
+    if(!this.isModified('password')) return next(); //si le mdp n'a ete modifier on passe a la suite
+
+    this.password = await bcrypt.hash(this.password,12);//bcrypt is asynch func  pass + cout
+
+    this.passwordConfirm = undefined; //on suprime le confirme pass de la bdd
+
+    next();
+})
+
+
+const User = mongoose.model("User", userShema);
 module.exports = User;
