09ecb99dd00c9200abb5e084517b0d4f653d3ee9
nicoWeb31 <nico.riot@free.fr>
2020-11-15 12:16:06 +0100

    custom err for mongooser

--- controllers/controllerTour.js
+++ controllers/controllerTour.js
@@ -53,9 +53,9 @@ exports.createNewTour = catchAsync(async (req, res, next) => {
 exports.getOneTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id);
 
-    // if(!tour){
-    //     return next(new AppErr('no tour found with that ID ! ',404))
-    // }
+    if(!tour){
+        return next(new AppErr('no tour found with that ID ! ',404))
+    }
 
     res.status(200).json({
         status: "success",
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,82 @@
-module.exports = (err, req, res, next) => {
-    
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+const AppErr = require('./../utils/AppErr');
 
+const handleCastError = err => {
+    const message = `Invalid ${err.path} : ${err.value}`;
+    return new AppErr(message,400);
+}
+
+const handleDuplicateFieldDB = err =>{
+    console.log(err)
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+    const message = `Duplicate field value : ${value} please use another value !`
+    return new AppErr(message,400);
+}
+
+const handleValidationErrorDB = err =>{
+    const errors = Object.values(err.errors).map(el=>el.message);
+    const message = `Invalid input Data. ${errors.join('. ')}`;
+    return new AppErr(message,400);
+}
+
+const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
+        error: err,
         message: err.message,
-    })
-}
+        stack: err.stack,
+        name: err.name
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    //Operational ,trusted error : send massage to client
+    if(err.isOperationnal){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    
+        //pregramming or other errors : don't leak deatail
+    }else{
+        //1) log error
+        console.error('ERROR 💣 ')
+
+        //2)send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong !!',
+        })
+    }
+
+};
+
+//les erreurs de moogose ne sont pas des erreurs operatinal
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+    // console.log(process.env.NODE_ENV)
+
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res);
+
+
+    } else if (process.env.NODE_ENV === "production") {
+
+        let error = {...err};
+        error.name = err.name;
+        error.errmsg = err.errmsg;
+        console.log(error.name)
+
+        if(error.name === 'CastError') {
+            error = handleCastError(error);
+        }
+        if(error.code === 11000) error = handleDuplicateFieldDB(error);
+        
+        if(error.name === 'ValidationError') error = handleValidationErrorDB(error)
+        
+        console.log(error);
+
+        sendErrorProd(error, res);
+    }
+};
