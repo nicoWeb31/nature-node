210b3a826441e85040ff8a63581600bbeab67895
nicoWeb31 <nico.riot@free.fr>
2020-11-24 10:06:20 +0100

    erxclude template

--- app.js
+++ app.js
@@ -77,6 +77,19 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get('/overview', (req, res) => {
+    res.status(200).render("overview",{
+        title: 'All tours'
+    })
+})
+
+app.get('/tour', (req, res) => {
+    res.status(200).render("tour",{
+        title: 'The forest hiker'
+    })
+})
+
+
 //------------------ROUTES Api---------------------------
 app.use("/api/v1/tours", require("./routes/tourRoutes"));
 app.use("/api/v1/users", require("./routes/userRoutes"));
