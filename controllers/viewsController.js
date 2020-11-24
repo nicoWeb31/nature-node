8bbbe1cd2610a3fb8fc333f69de462c4e39d2793
nicoWeb31 <nico.riot@free.fr>
2020-11-24 11:23:19 +0100

    overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,15 +1,19 @@
-exports.homePage = (req, res) => {
-    res.status(200).render("base", {
-        tour: "The forest ",
-        user: "toto",
-    });
-};
+const Tour = require("./../models/tourModel")
+const catchAsync = require('./../utils/catchAsync')
 
-exports.getOverview = (req, res) => {
+
+exports.getOverview = catchAsync(async(req, res, next) => {
+    //1) get tour from collection
+    const tours = await Tour.find();
+    
+    //2 Build templates
+    //3 render the template using data from 1
+
     res.status(200).render("overview", {
         title: "All tours",
+        tours: tours
     });
-};
+});
 
 exports.getOneTour = (req, res) => {
     res.status(200).render("tour", {
--- routes/viewsRoute.js
+++ routes/viewsRoute.js
@@ -4,9 +4,8 @@ const viewController = require('./../controllers/viewsController')
 
 const router = express.Router();
 
-router.get("/",viewController.homePage );
 
-router.get('/overview',viewController.getOverview )
+router.get('/',viewController.getOverview )
 
 router.get('/tour',viewController.getOneTour)
 
